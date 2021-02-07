afc874795d007db51659133ad2c4cf695281a9ef
Yash <yashyenugu@gmail.com>
2021-02-07 17:33:57 +0530

    Added code to standardize images

--- facenet.py
+++ facenet.py
@@ -1,3 +1,11 @@
 import numpy as np
 import tensorflow as tf
 import cv2
+
+def standardize(image):
+    mean = np.mean(image, axis=(1,2,3),keepdims=True)
+    std_dev = np.std(image,axis=(1,2,3),keepdims=True)
+    std_dev = np.maximum(std_dev,1.0/np.sqrt(image.size))
+
+    standardized_image = (image - mean)/std_dev
+    return standardized_image
